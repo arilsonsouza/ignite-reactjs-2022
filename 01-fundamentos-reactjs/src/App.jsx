b0c2d76634263a29b1bdf1c3a21fb64440e14985
Arilson Souza <arilsons61@gmail.com>
2023-01-11 22:24:21 -0300

    react component props

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,14 +1,14 @@
-import { Post } from './Post'
+import { Post } from "./Post";
 
 export function App() {
   return (
-   <>
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
-   </>
-  )
-}
+    <>
+      <Post
+        author="Arilson Souza"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi laborum quis suscipit aliquid reprehenderit veritatis quisquam dolore voluptates maxime nemo unde repudiandae doloribus labore, dolores odit perspiciatis cumque, soluta eaque?"
+      />
 
+      <Post author="Arilson Oliveira" content="Um novo post muito legal!" />
+    </>
+  );
+}
