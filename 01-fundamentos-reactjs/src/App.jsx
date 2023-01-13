88e2aba8d17f5a9b5bfb8b5f9ed2a37b0ead55d1
Arilson Souza <arilsons61@gmail.com>
2023-01-13 16:54:31 -0300

    add Sidebar component

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,16 +1,23 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Siderbar";
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Arilson Souza"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi laborum quis suscipit aliquid reprehenderit veritatis quisquam dolore voluptates maxime nemo unde repudiandae doloribus labore, dolores odit perspiciatis cumque, soluta eaque?"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Scarlet"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi laborum quis suscipit aliquid reprehenderit veritatis quisquam dolore voluptates maxime nemo unde repudiandae doloribus labore, dolores odit perspiciatis cumque, soluta eaque?"
+          />
 
-      <Post author="Arilson Oliveira" content="Um novo post muito legal!" />
+          <Post author="Scarlet" content="Um novo post muito legal!" />
+        </main>
+      </div>
     </>
   );
 }
