d12e3972df3332b7ba023ec122513658a9bebf24
Arilson Souza <arilsons61@gmail.com>
2023-12-06 18:20:56 -0300

    feat: add search and create transaction functionalities

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -1,19 +1,32 @@
+import { useState } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 import { HeaderContainer, HeaderContent, NewTransactionBtn } from './styles'
 import logoImg from '../../assets/logo.svg'
 import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
+  const [isTransactionModalOpen, setIsTransactionModalOpen] =
+    useState<boolean>(false)
+
+  function handleTransactionModalOpenChange(value: boolean) {
+    setIsTransactionModalOpen(value)
+  }
+
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <Dialog.Root>
+        <Dialog.Root
+          open={isTransactionModalOpen}
+          onOpenChange={handleTransactionModalOpenChange}
+        >
           <Dialog.Trigger asChild>
             <NewTransactionBtn>Nova transação</NewTransactionBtn>
           </Dialog.Trigger>
 
-          <NewTransactionModal />
+          <NewTransactionModal
+            handleTransactionModalOpenChange={setIsTransactionModalOpen}
+          />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
